9ba36d255a66419c078178e180495a990c892096
palaroda <30724055+palaroda@users.noreply.github.com>
2026-08-01 23:12:04 +0300

    SynRG

--- TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
@@ -21,25 +21,61 @@ public class LauncherIconController {
     public static boolean isEnabled(LauncherIcon icon) {
         Context ctx = ApplicationLoader.applicationContext;
         int i = ctx.getPackageManager().getComponentEnabledSetting(icon.getComponentName(ctx));
-        return i == PackageManager.COMPONENT_ENABLED_STATE_ENABLED || i == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT && icon == LauncherIcon.DEFAULT;
+        return i == PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                || i == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT && icon == LauncherIcon.DEFAULT;
     }
 
     public static void setIcon(LauncherIcon icon) {
         Context ctx = ApplicationLoader.applicationContext;
         PackageManager pm = ctx.getPackageManager();
         for (LauncherIcon i : LauncherIcon.values()) {
-            pm.setComponentEnabledSetting(i.getComponentName(ctx), i == icon ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
-                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+            pm.setComponentEnabledSetting(
+                    i.getComponentName(ctx),
+                    i == icon
+                            ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                            : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP
+            );
         }
     }
 
     public enum LauncherIcon {
-        DEFAULT("DefaultIcon", R.drawable.icon_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconDefault),
-        VINTAGE("VintageIcon", R.drawable.icon_6_background_sa, R.mipmap.icon_6_foreground_sa, R.string.AppIconVintage),
-        AQUA("AquaIcon", R.drawable.icon_4_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconAqua),
-        PREMIUM("PremiumIcon", R.drawable.icon_3_background_sa, R.mipmap.icon_3_foreground_sa, R.string.AppIconPremium),
-        TURBO("TurboIcon", R.drawable.icon_5_background_sa, R.mipmap.icon_5_foreground_sa, R.string.AppIconTurbo),
-        NOX("NoxIcon", R.mipmap.icon_2_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconNox);
+        DEFAULT(
+                "DefaultIcon",
+                android.R.color.white,
+                R.mipmap.icon_foreground_synrg_sa,
+                R.string.AppIconDefault
+        ),
+        VINTAGE(
+                "VintageIcon",
+                android.R.color.white,
+                R.mipmap.icon_6_foreground_synrg_sa,
+                R.string.AppIconVintage
+        ),
+        AQUA(
+                "AquaIcon",
+                android.R.color.white,
+                R.mipmap.icon_4_foreground_synrg_sa,
+                R.string.AppIconAqua
+        ),
+        PREMIUM(
+                "PremiumIcon",
+                android.R.color.white,
+                R.mipmap.icon_3_foreground_synrg_sa,
+                R.string.AppIconPremium
+        ),
+        TURBO(
+                "TurboIcon",
+                android.R.color.white,
+                R.mipmap.icon_5_foreground_synrg_sa,
+                R.string.AppIconTurbo
+        ),
+        NOX(
+                "NoxIcon",
+                android.R.color.white,
+                R.mipmap.icon_2_foreground_synrg_sa,
+                R.string.AppIconNox
+        );
 
         public final String key;
         public final int background;
